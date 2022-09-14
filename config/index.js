c3fcc497e3a92d0296245e5cd7838e32afe6437e
jerrysunsxx <113500540+jerrysunsxx@users.noreply.github.com>
2022-09-14 14:49:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oTsMm6sG_70b4llpI581HgePjQtw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "7MsVmsf8IMdHKpgOIKJ8QzGPQ0zlBnOipvRiW7jaPpg",
       // 所在省份
       province: "北京",
       // 所在城市
